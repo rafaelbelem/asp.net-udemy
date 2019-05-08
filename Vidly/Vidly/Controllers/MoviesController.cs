8bc0ab6cbc2ab7b9de4a2d6a4222c013a86c77c6
Rafael Avelar Alves Belem <rafael.belem@petrobras.com.br>
2019-05-08 17:56:10 -0300

    ASPNET UDEMY

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek"};
+            return View(movie);
+        }
+
+        public ActionResult Edit([Bind(Prefix = "id")] int movieid)
+        {
+            return Content("id=" + movieid);
+        }
+
+        public ActionResult Index (int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrEmpty(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleasedDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
